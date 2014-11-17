d72f8bcb8727bb0a6dd59459b6197eebc7624178
Melanie L. Fryman <melanie@frymanet.com>
2014-11-17 15:47:39 -0600

    task model complete

--- models/tasks.js
+++ models/tasks.js
@@ -1,9 +1,26 @@
-/**
- *
- * Created by mikeybadr on 11/17/14.
- */
 
+'use strict';
 
-function Task(){};
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema,
+    Priority = require('priority.model');
 
-module.exports = Task;
+var TaskSchema = new Schema({
+    name: String,
+    createdOn: { type: Date, default: Date.now },
+    dueOn: { type: Date },
+    priority: {type: mongoose.Schema.Types.ObjectId, ref: 'Priority'}
+});
+
+TaskSchema
+    .virtual('tasks')
+    .get(function() {
+        return {
+            'name': this.name,
+            'createdOn': this.createdOn,
+            'dueOn': this.dueOn,
+            'priority': this.priority
+        };
+    });
+
+module.exports = mongoose.model('Task', TaskSchema);
